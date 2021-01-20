9f1c5c12f35008a562f7728f651df2e31d2a8331
luisvinicius09 <luisvinicius0906@gmail.com>
2021-01-20 15:55:06 -0300

    Add env library & file

--- bin/ping.rb
+++ bin/ping.rb
@@ -1,6 +1,9 @@
 require 'discordrb'
+require 'dotenv/load'
 
-bot = Discordrb::Bot.new token: '', client_id: 
+Dotenv.load('tokens.env')
+
+bot = Discordrb::Bot.new token: ENV['SECRET_TOKEN'], client_id: ENV['CLIENT_ID']
 
 bot.message(with_text: 'Ping!') do |event|
   event.respond 'Pong!'
