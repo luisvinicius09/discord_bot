34ea98ed5c0322bbeb73fb0e5d15feb1f11a62a1
luisvinicius09 <luisvinicius0906@gmail.com>
2021-01-20 15:44:13 -0300

    Remove token

--- bin/ping.rb
+++ bin/ping.rb
@@ -1,6 +1,6 @@
 require 'discordrb'
 
-bot = Discordrb::Bot.new token: 'ODAxNDk5OTg0MzY5MDI1MTU1.YAhlBw.LyTFrlV9yPNfjtO1TFYxShooZCw', client_id: 801499984369025155
+bot = Discordrb::Bot.new token: '', client_id: 
 
 bot.message(with_text: 'Ping!') do |event|
   event.respond 'Pong!'
